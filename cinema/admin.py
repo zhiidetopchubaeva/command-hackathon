1352a685193b405057284d1d6797e40a8a1bb965
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-08-15 12:12:28 +0600

    admin

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Anime, Comment, Otzyv, Rating, Like
+
+admin.site.register(Category)
+admin.site.register(Anime)
+admin.site.register(Comment)
+admin.site.register(Rating)
+admin.site.register(Like)
+admin.site.register(Otzyv)
