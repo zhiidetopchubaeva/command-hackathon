901a6f17d358fa47fd81718b52cb03ebda348afa
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-08-15 14:17:33 +0600

    all

--- config/settings.py
+++ config/settings.py
@@ -111,6 +111,7 @@ SWAGGER_SETTINGS = {
     }
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -145,7 +146,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+import os 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,15 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+from config.settings import STATIC_ROOT
+
+
 schema_view = get_schema_view(
     openapi.Info(
-        title="Shop 21 API",
+        title="Cinema API",
         description="...",
         default_version="v1",
     ),
@@ -32,6 +38,13 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    # path('shop/', include('shop.urls')),
-    path('docs/', schema_view.with_ui("swagger"))
+    # path('anime/', include('cinema.urls')),
+    path('docs/', schema_view.with_ui("swagger")),
+    path('', include('cinema.urls')),
+
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
