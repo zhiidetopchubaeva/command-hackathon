d7a83fdd1f0eedcda4ce163cab75dc6f83726cda
Macbook <macbook@MacBook-Pro-Macbook-2.local>
2022-08-15 12:25:32 +0600

    serializer_project

---
+++ cinema/serializer.py
@@ -0,0 +1,43 @@
+from itertools import product
+from rest_framework import serializers
+
+from .models import Product, Comment, Like 
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+    
+    def to_representation(self, instance:Product):
+        rep = super().to_representation(instance)
+        rep["comments"] = CommentSerializer(instance.comments.all(), many=True).data
+        rep["likes"] = instance.likes.all().count()
+        rep["rating"] = instance.get_average_rating()
+        rep["liked_by_user"] = False
+        rep["user_rating"] = 0
+
+        request = self.context.get("request")
+
+        if request.user.is_authenticated:
+            rep["liked_by_user"] = Like.objects.filter(user=request.user, product=instance).exists()
+            if rating.objects.filter(user=request.user, product=instance).exists():
+                rating = rating.objects.get(user=request.user, product=instance)
+                rep["user_rating"] = rating.value
+
+        return rep
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        exclude = ['user']
+
+    def create(self, validated_data):
+        validated_data["user"] = self.context.get("request").user
+        return super().create(validated_data)
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["user"] = instance.user.email
+        return rep
+
