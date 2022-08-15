243ef1f49afd9b70849e9ff8b6dcfeaca4047128
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-08-15 18:59:22 +0600

    str

--- cinema/models.py
+++ cinema/models.py
@@ -7,15 +7,21 @@ User = get_user_model()
 class Category(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 
 class Anime(models.Model):
     categories = models.ManyToManyField(Category, related_name='anime')
     name = models.CharField(max_length=50)
     opisanie = models.TextField()
     seria = models.IntegerField()
-    year = models.DateTimeField()
+    year = models.DateField()
     image = models.ImageField(
-        upload_to="media/films")
+        upload_to="media")
+
+    def __str__(self):
+        return self.name
 
     @property
     def average_rating(self):
@@ -29,6 +35,9 @@ class Rating(models.Model):
     anime = models.ForeignKey(Anime, related_name='ratings', on_delete=models.CASCADE)
     value = models.IntegerField(choices=[(1,1), (2,2), (3,3), (4,4), (5,5)])
 
+    def __str__(self):
+        return f'{self.user}: {self.anime}. Rating: {self.value}'
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
@@ -36,10 +45,17 @@ class Comment(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.body
+
+
 
 class Like(models.Model):
     user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
     anime = models.ForeignKey(Anime, related_name='likes', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'LIKE TO ANIME: {self.anime}  FROM USER: {self.user}'
 
 class Otzyv(models.Model):
     user = models.ForeignKey(User, related_name='otzyv', on_delete=models.CASCADE)
@@ -47,3 +63,6 @@ class Otzyv(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     value = models.IntegerField(choices=[(5,5), (6,6), (7,7), (8,8), (9,9), (10,10)])
+
+    def __str__(self):
+        return f'REVIEW ON ANIME: {self.anime}.  FROM USER: {self.user}. TEXT OOF REVIEW: {self.body}'
