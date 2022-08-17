7df5577b1cfac4904351eea7c10c26553024ba40
ertayx <ertaiesen05@gmail.com>
2022-08-17 21:00:13 +0600

    chat

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterAPIView, activate
 
 
+
+from .views import test_view
 urlpatterns = [
+    path('test/', test_view),
+
+
     path('register/', RegisterAPIView.as_view()),
     path('activate/<str:activation_code>/', activate),
     path('api/token/', TokenObtainPairView.as_view()),
